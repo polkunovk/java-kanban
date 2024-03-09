b578bfd0ff901ab56fa07fdafad80761aac9166c
Konstantin <lenaaks@mail.ru>
2024-03-09 11:19:33 +0300

    Добавлены пустые строки

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,20 +2,36 @@ import java.util.*;
 
 interface TaskManager {
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
+
     Task getTaskById(int taskId);
+
     Epic getEpicById(int epicId);
+
     Subtask getSubtaskById(int subtaskId);
+
     void createEpic(String title, String description);
+
     void createSubtask(String title, String description, int epicId);
+
     void updateTaskStatus(int taskId, TaskStatus status);
+
     List<Subtask> getAllSubtasksOfEpic(int epicId);
+
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
+
     void deleteTask(int taskId);
+
     void deleteEpic(int epicId);
+
     void deleteSubtask(int subtaskId);
+
     List<Task> getHistory();
 }
