438f5f6c78b0fc272e3337ab854b713dd899c8ed
Konstantin <lenaaks@mail.ru>
2024-03-09 12:20:25 +0300

    Добавлены пустые строки

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -2,8 +2,10 @@ import java.util.List;
 
 public interface HistoryManager {
 
+    // Пустая строка добавлена!
     void add(Task task);
 
+    // Пустая строка добавлена!
     void remove(int id);
 
     List<Task> getHistory();
