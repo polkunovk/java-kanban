912bc0dad41a68ad7ec4c1573d9cc122148bf35d
Konstantin <lenaaks@mail.ru>
2024-03-17 23:41:18 +0300

    Спринт 7

--- src/FileBackedTaskManagerTest.java
+++ src/FileBackedTaskManagerTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
