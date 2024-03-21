928222f19b72a6e3ad27c1592d88d2e96b1a1df8
Konstantin <lenaaks@mail.ru>
2024-03-21 20:37:36 +0300

    Исправление замечаний 2.2

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-// import java.util.Objects;
 
 public class InMemoryTaskManager implements TaskManager {
     private int idCounter = 1;
