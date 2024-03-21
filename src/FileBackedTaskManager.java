b08d9e813928c237f34a440c16aab610453a7c0c
Konstantin <lenaaks@mail.ru>
2024-03-21 22:22:31 +0300

    Исправление замечаний 2.5

--- src/FileBackedTaskManager.java
+++ src/FileBackedTaskManager.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,13 +1,24 @@
 import java.util.List;
+
 public interface TaskManager {
+
     List<Task> getAllTasks();
+
     Task getTaskById(int taskId);
+
     void createEpic(String title, String description);
+
     void createSubtask(String title, String description, int epicId);
+
     void updateTaskStatus(int taskId, TaskStatus status);
+
     List<Subtask> getAllSubtasksOfEpic(int epicId);
+
     void deleteEpic(int epicId);
+
     void deleteSubtask(int subtaskId);
+
     List<Task> getHistory();
+
     void addTask(Task task);
 }
