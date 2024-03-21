6a7760b79ecb6e2c5c1acd0602e2a0c797d409e9
Konstantin <lenaaks@mail.ru>
2024-03-21 23:45:16 +0300

    Исправление замечаний 2.7

--- src/FileBackedTaskManagerTest.java
+++ src/FileBackedTaskManagerTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -18,25 +16,6 @@ public class FileBackedTaskManagerTest {
         FileBackedTaskManager loadedTaskManager = FileBackedTaskManager.loadFromFile(saveFile, new InMemoryHistoryManager());
 
         assertTrue(loadedTaskManager.getAllTasks().isEmpty());
-    }
-
-    @Test
-    public void saveAndLoadMultipleTasks() {
-        File saveFile = createTempFile();
-
-        FileBackedTaskManager taskManager = new FileBackedTaskManager(new InMemoryHistoryManager(), saveFile);
-        taskManager.createEpic("Эпик 1", "Описание Эпика 1");
-        taskManager.createSubtask("Подзадача 1", "Описание Подзадачи 1", 1);
-        taskManager.createSubtask("Подзадача 2", "Описание Подзадачи 2", 1);
-        taskManager.saveToFile();
-
-        FileBackedTaskManager loadedTaskManager = FileBackedTaskManager.loadFromFile(saveFile, new InMemoryHistoryManager());
-
-        int epicsCount = loadedTaskManager.getAllTasks().stream().filter(task -> task instanceof Epic).mapToInt(task -> 1).sum();
-        int subtasksCount = loadedTaskManager.getAllTasks().stream().filter(task -> task instanceof Subtask).mapToInt(task -> 1).sum();
-
-        assertEquals(1, epicsCount);
-        assertEquals(2, subtasksCount);
     }
 
     private File createTempFile() {
