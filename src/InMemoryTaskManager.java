6cdc253718f44c1dd9c794bf9518063f7a2c513c
Konstantin <lenaaks@mail.ru>
2024-04-03 23:18:27 +0300

    Спринт 8

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -92,7 +92,7 @@ public class InMemoryTaskManager implements TaskManager {
         if (epic != null) {
             List<Subtask> epicSubtasks = epic.getSubtasks();
             for (Subtask subtask : epicSubtasks) {
-                subtasks.remove(subtask.getId());
+                historyManager.remove(subtask.getId()); 
             }
             epics.remove(epicId);
             historyManager.remove(epicId);
