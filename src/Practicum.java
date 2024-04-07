2f99106b4dc465b4b517da200514d1cf6d651405
Konstantin <lenaaks@mail.ru>
2024-04-08 02:28:27 +0300

    В процессе

--- src/Practicum.java
+++ src/Practicum.java
@@ -2,64 +2,6 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 
-class LikesInfo {
-    private boolean hasOwnerLiked;
-    private Like[] likes;
-    private int repostsCount;
-
-    public boolean isHasOwnerLiked() {
-        return hasOwnerLiked;
-    }
-
-    public void setHasOwnerLiked(boolean hasOwnerLiked) {
-        this.hasOwnerLiked = hasOwnerLiked;
-    }
-
-    public Like[] getLikes() {
-        return likes;
-    }
-
-    public void setLikes(Like[] likes) {
-        this.likes = likes;
-    }
-
-    public int getRepostsCount() {
-        return repostsCount;
-    }
-
-    public void setRepostsCount(int repostsCount) {
-        this.repostsCount = repostsCount;
-    }
-}
-
-class Like {
-    private String name;
-    private String avatarUrl;
-
-    public Like() {}
-
-    public Like(String name, String avatarUrl) {
-        this.name = name;
-        this.avatarUrl = avatarUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-}
-
 public class Practicum {
 
 
